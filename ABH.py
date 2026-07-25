c0d4a8a0e3b23833ccb3a79c84fd93998214cc95
VIPABH <a891983.2@gmail.com>
2026-07-25 13:04:42 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -1,4 +1,4 @@
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 API_ID = 37569690 
 API_HASH = '9ab40e3001a1562320803b5be438786a'
 ABH = TelegramClient('didi_user_ultra', API_ID, API_HASH)
